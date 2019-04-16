a5805afa5c9c1353d963448e05841fa7091b7c94
zhangzhihui <zhang.zhihui@intellif.com>
2019-04-16 09:29:12 +0800

    先不搞那么复杂，去掉中间部分

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -3,9 +3,6 @@
     <div id="sidebarWrapper">
       <v-sidebar />
     </div>
-    <div id="listWrapper">
-      <v-list :lists="lists" />
-    </div>
     <div id="contentWrapper">
       <v-button-group />
       <router-view></router-view>
@@ -15,7 +12,6 @@
 
 <script>
 import Sidebar from '@/containers/Sidebar/Sidebar';
-import List from '@/components/List/List';
 import ButtonGroup from '@/components/ButtonGroup/ButtonGroup';
 export default {
   name: "elec-z",
@@ -26,7 +22,6 @@ export default {
   },
   components: {
     'v-sidebar': Sidebar,
-    'v-list': List,
     'v-button-group': ButtonGroup
   }
 };
@@ -34,7 +29,6 @@ export default {
 
 <style lang="scss">
 $sidebar-width: 60px;
-$center-list-width: 250px;
 * {
   box-sizing: border-box;
   margin: 0;
@@ -57,19 +51,10 @@ body,
   left: 0;
   bottom: 0;
 }
-#listWrapper {
-  width: $center-list-width;
-  height: 100%;
-  background: #e9e7e8;
-  position: absolute;
-  top: 0;
-  left: $sidebar-width;
-  bottom: 0;
-}
 #contentWrapper {
   position: absolute;
   top: 0;
-  left: $sidebar-width + $center-list-width;
+  left: $sidebar-width;
   right: 0;
   bottom: 0;
 }
